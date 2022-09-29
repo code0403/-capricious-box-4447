1ab182f338c329865d40ad638f9af9c851b10f98
Abhishek Singh <sabhishek900@gmail.com>
2022-09-30 00:41:55 +0530

    homapage completed

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-document.querySelector("#openIcon").addEventListener("click",openNav);
+// let open=document.querySelector("#openIcon")
+// open.addEventListener("click",openNav);
+
     function openNav(){
-      document.getElementById("mysidenav").style.width ="100";
+      document.getElementById("mysidenav").style.width ="250px";
     }
     
     function closeNav(){
         document.getElementById("mysidenav").style.width = "0";
     }
+
+    // dropdown
+
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+     for (i = 0; i < dropdown.length; i++) {
+       dropdown[i].addEventListener("click", function() {
+        //console.log("hello")
+       this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+       if (dropdownContent.style.display === "block") {
+         dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
